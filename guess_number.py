7b07f60c84a7db06d54dd9e0c17ee9be2432840c
B1t-Master <isaacmwanginjuguna@gmail.com>
2026-09-23 12:17:31 +0300

    feat: terminal guessing game

---
+++ guess_number.py
@@ -0,0 +1,76 @@
+from random import choice
+from sys import exit
+
+
+def playGame(name="player 1"):
+    win_percentage = player_wins = total_rounds = 0
+    print(f"Welcome {name} to the guessing game\n")
+
+    def calc_win_percentage():
+        nonlocal win_percentage
+        nonlocal player_wins
+        nonlocal total_rounds
+
+        win_percentage = f"{player_wins/total_rounds:.2%}"
+
+        return win_percentage
+
+    def get_computer_choice():
+        computer_choice = choice("1,2,3")
+        return computer_choice
+
+    def get_player_choice():
+        print(
+            "Guess which number I have in mind 1, 2 or 3...\nq - quit ")
+        player_choice = input()
+
+        if player_choice == "q":
+            exit("Thank you for playing")
+        return player_choice
+
+    def is_player_choice_valid(player_choice):
+        if player_choice not in ["1", "2", "3", "q"]:
+            print("You chose and invalid choice try again!\n")
+            return play_round()
+
+    def play_round(player_choice=True):
+        nonlocal name
+        nonlocal player_wins
+        nonlocal total_rounds
+        # player_choice = get_player_choice()
+
+        # is_player_choice_valid(player_choice)
+
+        while (player_choice):
+            player_choice = get_player_choice()
+            is_player_choice_valid(player_choice)
+
+            total_rounds += 1
+
+            if player_choice == get_computer_choice():
+                player_wins += 1
+                print(
+                    f"\n{name} wins  \nYou have won that round 🎉\nyour current win percentage is {calc_win_percentage()} \n")
+            else:
+                print(
+                    f"\ncomputer wins 🐍 \nSorry you lost that round 😭\nyour current win percentage is {calc_win_percentage()} \n")
+
+    return play_round
+
+
+if __name__ == "__main__":
+
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="This is the guess my number game")
+
+    parser.add_argument("-n", "--name", metavar="name", required=True,
+                        help="Your name for a personalized game experiece")
+
+    args = parser.parse_args()
+    # message = f"{args.name}"
+    # print(message)
+
+    play_guess_number = playGame(args.name)
+
+    play_guess_number()
